00befb768e7c06cc5d68005f15177ca006181df4
beyene <beyenenw@gmail.com>
2017-06-27 22:32:49 -0700

    updated these files

--- rgbcolor_game.js
+++ rgbcolor_game.js
@@ -1,33 +1,37 @@
-
-    var colors=[
-    	"rgb(255, 0, 0)  ",
-    	"rgb(255, 255, 0)  ",
+ var colors=[
+    	"rgb(255, 0, 0)",
+    	"rgb(255, 255, 0)",
     	"rgb(0, 255, 0) ",
-    	"rgb(0, 255, 255)  ",
-    	"rgb(0, 0, 255)  ",
-    	"rgb(255, 0, 255)  "   
+    	"rgb(0, 255, 255)",
+    	"rgb(0, 0, 255)",
+    	"rgb(255, 0, 255)"   
     ];
-    var square=document.querySelectorAll(".square");
+
+    var squares=document.querySelectorAll(".square");
     var pickedColor = colors[3];
     var colorDisplay=document.getElementById("colorDisplay");
     
+    colorDisplay.textContent=pickedColor;
       
-    for(var i=0;i<square.length;i++){
-    //add initial colors to squares
-    	square[i].style.backgroundColor=colors[i];
-    	//add click linstener
-    	square[i].addEventListener("click", function(){	
-    		//grab color of picked square
+    for(var i=0;i<squares.length;i++){
+            //add initial colors to squares
+    	squares[i].style.backgroundColor=colors[i];
+    	
+            //add click linstener
+    	squares[i].addEventListener("click", function(){	    		
+                        //grab color of picked squares
     		var clickedColor=this.style.backgroundColor;
     		//compare color to pickedColor
-    		if(pickedColor===clickedColor){
-    			alert("Correct");
-    			//console.log(pickedColor);
+    		if(clickedColor===pickedColor){
+    			alert("Correct!");
+    			console.log(pickedColor);
     		}else{
     			alert("wrong!!!");
     		}
     	});
     }
     
+    
+
        
  
